a4bc03a410a148e965c2f80dd89d743877a5fa29
Kenth Fagerlund <kenth@divit.com.hk>
2022-03-11 21:59:16 +0800

    Outline of the cube

--- contracts/cube.sol
+++ contracts/cube.sol
@@ -10,52 +10,65 @@
 // Developed by Kenth Fagerlund (https://github.com/arkalon76)
 // Inspired by the fantastic work by Dogu Deniz UGUR (https://github.com/DoguD)
 pragma solidity ^0.8.9;
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
-import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
-import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract cube is
-  Initializable,
-  PausableUpgradeable,
-  AccessControlUpgradeable,
-  UUPSUpgradeable {
-  using SafeMath for uint256;
+    Initializable,
+    PausableUpgradeable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable
+{
+    using SafeMath for uint256;
 
-   bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
-  bytes32 public constant UPGRADER_ROLE = keccak256('UPGRADER_ROLE');
-  bytes32 public constant MANAGER_ROLE = keccak256('MANAGER_ROLE');
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
+    /**
+      This will dictate what funding instrument it accepts and will distribute to investment vehicles.
+      Side effect of this, is that it will not be able to fund vehicle with different instruments.
+     */
+    address fundingInstrument;
 
-  constructor() initializer {}
+    constructor() initializer {}
 
-    function initialize() public initializer {
-    __Pausable_init();
-    __AccessControl_init();
-    __UUPSUpgradeable_init();
+    function initialize(address _fundingInstrument) public initializer {
+        __Pausable_init();
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
 
-    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    _grantRole(PAUSER_ROLE, msg.sender);
-    _grantRole(UPGRADER_ROLE, msg.sender);
-    _grantRole(MANAGER_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+        _grantRole(MANAGER_ROLE, msg.sender);
 
-  }
+        fundingInstrument = _fundingInstrument;
+    }
 
-  function pause() public onlyRole(PAUSER_ROLE) {
-    _pause();
-  }
+    // Queue: Targets
+    // Target: amount, address
 
-  function unpause() public onlyRole(PAUSER_ROLE) {
-    _unpause();
-  }
+    function pushInvestTarget() external virtual;
 
-  function _authorizeUpgrade(address newImplementation)
-    internal
-    override
-    onlyRole(UPGRADER_ROLE)
-  {}
+    function popInvestTarget() external virtual;
 
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(UPGRADER_ROLE)
+    {}
 }
---
+++ test/InvestmentVehicle/upgrade-test.js
@@ -0,0 +1,55 @@
+// Copyright 2022 Kenth Fagerlund.
+// SPDX-License-Identifier: MIT
+const { expect } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+const { BN, expectEvent, expectRevert } = require("@openzeppelin/test-helpers");
+const { inTransaction } = require("@openzeppelin/test-helpers/src/expectEvent");
+
+let vehicleContract;
+let usdtContract;
+let owner;
+let addr1;
+let addr2;
+let addrs;
+let MANAGER_ROLE;
+let UPGRADER_ROLE;
+let PAUSER_ROLE;
+
+beforeEach(async function () {
+  [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+  MANAGER_ROLE = ethers.utils.id("MANAGER_ROLE");
+  UPGRADER_ROLE = ethers.utils.id("UPGRADER_ROLE");
+  PAUSER_ROLE = ethers.utils.id("PAUSER_ROLE");
+
+  const USDT = await ethers.getContractFactory("Tether");
+  usdtContract = await USDT.deploy();
+  await usdtContract.deployed();
+
+  // Get the ContractFactory and Signers here.
+  const Vehicle = await ethers.getContractFactory("InvestmentVehicle");
+  vehicleContract = await upgrades.deployProxy(Vehicle, [usdtContract.address, 100], {
+    initializer: "initialize",
+    kind: "uups",
+  });
+  await vehicleContract.deployed();
+});
+
+describe("Upgrading contract", () => {
+  it("Should upgrade contract without modifying data", async () => {
+    // Adding test data in v1 contract
+    await usdtContract.approve(vehicleContract.address, 2000);
+    expect(await usdtContract.allowance(owner.address, vehicleContract.address)).to.equal(2000);
+    await expect(() => vehicleContract.depositInvestment(owner.address, 100)).to.changeTokenBalance(
+      usdtContract,
+      vehicleContract,
+      100
+    );
+    expect(await vehicleContract.investmentPool()).to.equal(100)
+
+    const v2 = await ethers.getContractFactory("InvestmentVehicleV2UpgradeTest");
+    const upgraded = await upgrades.upgradeProxy(vehicleContract.address, v2);
+    await expect(upgraded.depositInvestment(owner.address, 100)).to.be.revertedWith('TESTING CONTRACT, DO NOT USE IN PROD')
+    expect(await upgraded.investmentPool()).to.equal(100)
+  });
+});
