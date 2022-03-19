93b8d06e7ff46b1d72d67ec24176925bebd6dd21
Kenth Fagerlund <kenth@divit.com.hk>
2022-03-20 07:18:28 +0800

    Allow for multi vehicle testing

--- scripts/admin-local.js
+++ scripts/admin-local.js
@@ -1,8 +1,9 @@
 // Copyright 2022 Kenth Fagerlund.
 // SPDX-License-Identifier: MIT
 const CUBE_ADDRESS = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0'
-const VEHICLE_ADDRESS = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9'
 const TOKEN_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+const STRONG_NODE = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9'
+const YIELD_FARM = '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'
 
 async function makeCubeOwnerOfVehicle(cubeContract, vehicleContract) {
     const [deployer] = await ethers.getSigners();
@@ -19,15 +20,21 @@ async function setInvestmentVehicleForCube(cubeContract, vehicleContract, target
     const Cube = await ethers.getContractFactory("cube");
     const cube = await Cube.attach(cubeContract);
     await cube.setInvestmentTarget(vehicleContract, targetAmountWei)
-    const result = await cube.currentVehicle()
-    const amount = await cube.currentTargetAmount()
-    console.log("Target vehicle is set to : ", result)
-    console.log("Target amount is ", amount)
+    const currentVehicle = await cube.currentVehicle()
+    const currentAmount = await cube.currentTargetAmount()
+    const nextVehicle = await cube.nextVehicle()
+    const nextAmount = await cube.nextTargetAmount()
+    console.log("Target vehicle is set to : ", currentVehicle)
+    console.log("Target amount is ", currentAmount)
+    console.log("Next vehicle is set to : ", nextVehicle)
+    console.log("Next amount is ", nextAmount)
 }
 
 async function main() {
-    await setInvestmentVehicleForCube(CUBE_ADDRESS, VEHICLE_ADDRESS, ethers.utils.parseUnits("800.0", 6))
-    await makeCubeOwnerOfVehicle(CUBE_ADDRESS, VEHICLE_ADDRESS)
+    await setInvestmentVehicleForCube(CUBE_ADDRESS, STRONG_NODE, ethers.utils.parseUnits("800.0", 6))
+    await setInvestmentVehicleForCube(CUBE_ADDRESS, YIELD_FARM, ethers.utils.parseUnits("1200.0", 6))
+    await makeCubeOwnerOfVehicle(CUBE_ADDRESS, STRONG_NODE)
+    await makeCubeOwnerOfVehicle(CUBE_ADDRESS, YIELD_FARM)
 }
 
 main()
